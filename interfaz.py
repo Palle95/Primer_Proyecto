07c461927092605da8b83b7b8b28849ad38367d1
Palle95 <agustinpalleres2@gmail.com>
2023-06-14 20:03:37 -0300

    una correccion

--- interfaz.py
+++ interfaz.py
@@ -1,11 +1,10 @@
 interfaz = open ("interfaz.txt", "w")
-interfaz.write("programa para ingresar datos")
+interfaz.write("programa para ingresar datos \n")
 
-nombre = input("ingresar su nombre aquí: ")
-edad = input("ingresar edad aquí: ")
+nombre = input("ingresar su nombre aquí: \n")
+edad = input("ingresar edad aquí: \n")
 
 interfaz.write(nombre)
 interfaz.write(edad)
 interfaz.close()
-
 
