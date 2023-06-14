79e889b126274008618d517ae067ee3b7f3beeaa
Palle95 <agustinpalleres2@gmail.com>
2023-06-14 17:21:21 -0300

    se agrega el proyecto

--- interfaz.py
+++ interfaz.py
@@ -1,17 +1,11 @@
 interfaz = open ("interfaz.txt", "w")
-
-
-newwork.write("programa para ingresar datos")
+interfaz.write("programa para ingresar datos")
 
 nombre = input("ingresar aquí: ")
-edad = int(input("ingresar edad aquí: "))
+edad = input("ingresar edad aquí: ")
 
-interfaz = open ("nterfaz.txt","r")
-nombre = interfaz.read()
-edad = interfaz.read()
-
-print (nombre,edad)
-
+interfaz.write(nombre)
+interfaz.write(edad)
 interfaz.close()
 
 
